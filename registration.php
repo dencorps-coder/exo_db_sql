569e775c2639dcc32ded8c40dd0eed137c322ea6
dencorps-codeur <mickael.roze@laplateforme.io>
2025-08-06 14:20:59 +0200

    exo sql phase2

--- registration.php
+++ registration.php
@@ -1,27 +1,43 @@
-<?
+<?php
+$erreur = [];
 
-    if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $name = $_POST["name"] ?? '';
-    $first_name = $_POST["first_name"] ?? '';
-    $mail = $_POST["mail"] ?? '';
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = trim(htmlspecialchars($_POST["name"] ?? ''));
+    $first_name = trim(htmlspecialchars($_POST["first_name"] ?? ''));
+    $mail = trim(htmlspecialchars($_POST["mail"] ?? ''));
     $password = $_POST["password"] ?? '';
 
-    $erreur = [];  
-   if (empty($name)) {
-        $erreur[] = "ce champ doit etre rempli";
-   }
-    if(empty($first_name)) { 
-        $erreur[] = "ce champ doit etre rempli";
+    // Vérification du nom
+    if (empty($name)) {
+        $erreur[] = "Le nom doit être rempli";
+    } else {
+        if (strlen($name) < 3) {
+            $erreur[] = "Veuillez mettre plus de caractères dans le nom";
+        }
+        if (strlen($name) > 55) {
+            $erreur[] = "Limite de caractères dépassée pour le nom";
+        }
     }
-    if(empty($mail)) 
-        $erreur[] = "ce champ doit etre rempli";
 
-    if(empty($password)) {
-        $erreur[] = "ce champ doit etre rempli";
-}
+    // Vérification du prénom
+    if (empty($first_name)) {
+        $erreur[] = "Le prénom doit être rempli";
+    }
 
+    // Vérification de l'email
+    if (empty($mail)) {
+        $erreur[] = "L'email doit être rempli";
+    } elseif (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        $erreur[] = "Votre adresse email n'est pas valide";
+    }
+
+    // Vérification du mot de passe
+    if (empty($password)) {
+        $erreur[] = "Le mot de passe doit être rempli";
+    }
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="fr">
